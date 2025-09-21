428235ca55a84289f4857eaf552bad89efb53ee2
Vittoria Stavolone <vittoria25.stavolone@gmail.com>
2025-09-21 22:22:56 +0200

    Update FastF1-Fastest_Lap_Analysis.py

--- FastF1-Fastest_Lap_Analysis.py
+++ FastF1-Fastest_Lap_Analysis.py
@@ -16,21 +16,21 @@ laps_bah = session_bah.laps                        # full lap data for Bahrain
 leclerc_laps_bah = laps_bah.pick_driver("LEC")     # filter only Leclerc’s laps
 fastest_bah = leclerc_laps_bah.pick_fastest()      # fastest lap of that driver
 print("Bahrain Race – LEC fastest lap:\n", fastest_bah)
-laps_bah.to_csv("bahrain_2025_laps.csv", index=False)  # save all laps to CSV
+laps_bah.to_csv("bahrain_2025_laps.csv", index=False)  # save all laps to CSV: "https://drive.google.com/file/d/1CcbIXF-UFSGeM3NUSP7UBcCh5nimdLLG/view?usp=sharing"
 
 # Block: Extract fastest lap for Leclerc in Monaco GP
 laps_mon = session_mon.laps
 leclerc_laps_mon = laps_mon.pick_driver("LEC")
 fastest_mon = leclerc_laps_mon.pick_fastest()
 print("Monaco Race – LEC fastest lap:\n", fastest_mon)
-laps_mon.to_csv("monaco_2025_laps.csv", index=False)
+laps_mon.to_csv("monaco_2025_laps.csv", index=False)  # save all laps to CSV: "https://drive.google.com/file/d/1uYYbdtTtJFfzSpWQkii8O97RC9OzM7MF/view?usp=sharing"
 
 # Block: Extract fastest lap for Leclerc in Silverstone GP
 laps_sil = session_sil.laps
 leclerc_laps_sil = laps_sil.pick_driver("LEC")
 fastest_sil = leclerc_laps_sil.pick_fastest()
 print("Silverstone Race – LEC fastest lap:\n", fastest_sil)
-laps_sil.to_csv("silverstone_2025_laps.csv", index=False)
+laps_sil.to_csv("silverstone_2025_laps.csv", index=False)  # save all laps to CSV: "https://drive.google.com/file/d/1Jm7BBG90PoIugUCjwD9SEt-nku_kaijg/view?usp=sharing"
 
 
 
