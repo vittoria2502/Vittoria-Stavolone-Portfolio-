c46cdc294c4ae0bcc9d1ffddf3677617f9618a26
Vittoria Stavolone <vittoria25.stavolone@gmail.com>
2025-09-24 23:01:07 +0200

    Update FastF1 - Fastest_Lap_Telemetry_Comparison.py

--- Step 2/FastF1 - Fastest_Lap_Telemetry_Comparison.py
+++ Step 2/FastF1 - Fastest_Lap_Telemetry_Comparison.py
@@ -36,13 +36,14 @@ delta_time, ref_tel, compare_tel = utils.delta_time(fast_leclerc, fast_verstappe
 fig, ax = plt.subplots(2, 1, figsize=(18, 16))  # 2 rows, 1 column
 
 # 1) Delta line (gap vs distance)
-ax[0].plot(ref_tel['Distance'], delta_time, color='yellow')
+ax[0].plot(ref_tel['Distance'], delta_time, color=rbr_color, label="Gap to LEC")
 ax[0].set_xlabel("Distance (m)")
-ax[0].set_ylabel("Gap to VER (s)")
+ax[0].set_ylabel("Delta (s)")
 ax[0].set_title("Delta Verstappen vs Leclerc Bahrain GP - Qualifying")
 for x, lab in zip(corner_positions, corner_labels):
     ax[0].axvline(x=x, color="white", linestyle="--", alpha=0.5)
     ax[0].text(x, 0.02, lab, color="lime", ha="left", va="center", transform=ax[0].get_xaxis_transform())
+ax[0].axhline(y=0, color="white", linestyle="--", alpha=0.5)
 
 # 2) Speed traces comparison
 # Leclerc speed telemetry
@@ -51,7 +52,7 @@ ax[1].plot(leclerc_car_data["Distance"], leclerc_car_data["Speed"], color=fer_co
 ax[1].plot(verstappen_car_data["Distance"], verstappen_car_data["Speed"], color=rbr_color, label="VER")
 ax[1].set_xlabel("Distance (m)")
 ax[1].set_ylabel("Speed (Km/h)")
-ax[1].set_title("Fastest lap comparison Bahrain GP - Qualifying")
+ax[1].set_title("Fastest lap Verstappen vs Leclerc Bahrain GP - Qualifying")
 for x, lab in zip(corner_positions, corner_labels):
     ax[1].axvline(x=x, color="white", linestyle="--", alpha=0.5)
     ax[1].text(x, 0.02, lab, color="lime", ha="left", va="center", transform=ax[1].get_xaxis_transform())
